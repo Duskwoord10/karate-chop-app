6e2899e89054d7c0d6fa4cd2b9f99b21961f19b0
Albert Sanfle <sanflealbert@gmail.com>
2024-07-11 10:57:19 -0400

    Add project files.

---
+++ KarateChopApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using KarateChopDomain;
+
+namespace KarateChopApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+=====> Karate Chop App <=====+\n\n");
+            Console.Write("Sorted array: | ");
+
+            int[] sortedArray = { 1, 3, 5, 7, 9, 11, 13, 15 };
+
+            foreach (var item in sortedArray)
+            {
+                Console.Write($"{item} | ");
+            }
+
+            Console.WriteLine("\n");
+
+            while (true)
+            {
+                Console.WriteLine("Choose a target to search in the array (or Crtl + C to exit): ");
+                Console.Write("=> ");
+
+                if (int.TryParse(Console.ReadLine(), out int target))
+                {
+                    int result = KarateChop.Chop(target, sortedArray);
+
+                    if (result != -1)
+                        Console.WriteLine($"Target {target} found at index {result}.\n");
+                    else
+                        Console.WriteLine($"Target {target} not found in the array.\n");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer target to search in the array.\n\n");
+                }
+            }
+        }
+    }
+}
